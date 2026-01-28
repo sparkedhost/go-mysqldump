737f07be7790b1e8466dedb9e8042eae1970c1d6
BetTD <between13131@gmail.com>
2026-01-28 01:44:23 +0100

    Support unsigned integer types

--- dump.go
+++ dump.go
@@ -446,6 +446,8 @@ func reflectColumnType(tp *sql.ColumnType) reflect.Type {
 	switch tp.ScanType().Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		return reflect.TypeOf(sql.NullInt64{})
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64: // unsigned integers
+		return reflect.TypeOf(sql.NullInt64{})
 	case reflect.Float32, reflect.Float64:
 		return reflect.TypeOf(sql.NullFloat64{})
 	case reflect.String:
